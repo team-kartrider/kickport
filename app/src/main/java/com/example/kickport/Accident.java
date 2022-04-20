392cf5ef261d7f14a3f4888a0b291778ea39c0a2
Lee-SungMin <saevers26@gmail.com>
2022-04-20 12:59:51 +0900

    add: show message or activity when press back button

--- app/src/main/java/com/example/kickport/Accident.java
+++ app/src/main/java/com/example/kickport/Accident.java
@@ -1,8 +1,5 @@
 package com.example.kickport;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -10,6 +7,9 @@ import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -122,5 +122,11 @@ public class Accident extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(Accident.this, MainActivity.class);
+        startActivity(intent);
     }
 }
